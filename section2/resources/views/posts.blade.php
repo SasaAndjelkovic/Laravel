bb52a2bf9562f5b067dffe2412471994d39aa20e
SasaAndjelkovic <sandjelkovickg@gmail.com>
2023-04-22 19:21:53 +0200

    basic blade

--- section2/resources/views/posts.blade.php
+++ section2/resources/views/posts.blade.php
@@ -2,17 +2,24 @@
     
     <body>
 
-        <?php foreach ($posts as $post) : ?>
-        <article>
+        <!-- <//?php foreach ($posts as $post) : ?> -->
+        @foreach ($posts as $post)
+            <!-- @dd($loop) -->
+        <article class="{{ $loop->even ? 'foobar' : '' }}">
            <h1>
-                <a href="/posts/<?= $post->slug?>">
-                    <?= $post->title; ?>
+                <a href="/posts/{{ $post->slug }}">
+                <!-- <a href="/posts/<//?= $post->slug?>"> -->
+                    <!-- <//?= $post->title; ?> -->
+                    <!-- <//bn?php echo $post->title; ?> -->
+                    {{ $post->title }}
                 </a>
             </h1>  
            <div>
-                <?= $post->excerpt; ?>
+                <!-- <//?= $post->excerpt; ?> -->
+                {{ $post->excerpt}}
            </div>
         </article>
-        <?php endforeach; ?>
+        <!-- <//?php endforeach; ?> -->
+        @endforeach
     </body>
  
