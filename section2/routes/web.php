a8e0f857c0acca871b1edf49d22e747ccf27607c
SasaAndjelkovic <sandjelkovickg@gmail.com>
2023-04-22 11:16:59 +0200

    reinstalacija yaml-front-matter uz izbegavanje greske u folderima

--- section2/routes/web.php
+++ section2/routes/web.php
@@ -1,10 +1,23 @@
 <?php
 
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use Illuminate\Support\Facades\File;
 
-Route::get('/', function () 
-{
+
+Route::get('/', function () {
+    $document = Spatie\YamlFrontMatter\YamlFrontMatter::parseFile(resource_path('posts/my-fourth-post.html'));
+
+    dd($document);
+
+    // $files = File::files(resource_path("posts"));
+    // $documents = [];
+
+    // foreach($files as $file) {
+    //     $documents[] = YamlFrontMatter::parseFile($file);
+    // }
+    
     $posts = Post::all();
 
     //dd($posts[0]->getContents());
@@ -12,9 +25,13 @@ Route::get('/', function ()
     return view('posts', [
         'posts' => $posts
     ]);
+  
 });
 
+
+
 Route::get('posts/{post}', function($slug) 
+//Find a post by its slug and pass it to a view called "post"
 {    
     $post = Post::find($slug);
 
