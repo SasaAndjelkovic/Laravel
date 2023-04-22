6e2a7c008d8a00a525315f9408064e0da370d2ed
SasaAndjelkovic <sandjelkovickg@gmail.com>
2023-04-22 20:44:45 +0200

    blade layout second way

--- section2/resources/views/layout.blade.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-
-<title>My blog</title>
-<link rel="stylesheet" href="app.css">
-
-<body>
-    @yield('content')
-</body>
--- section2/resources/views/posts.blade.php
+++ section2/resources/views/posts.blade.php
@@ -1,20 +1,22 @@
-@extends ('layout')
-
-@section ('content')
+<x-layout>
 
     @foreach ($posts as $post)
-    <article class="{{ $loop->even ? 'foobar' : '' }}">
-    <h1>
-            <a href="/posts/{{ $post->slug }}">
-                {{ $post->title }}
-            </a>
-        </h1>  
-    <div>
-            {{ $post->excerpt}}
-    </div>
-    </article>
+        <article class="{{ $loop->even ? 'foobar' : '' }}">
+            <h1>
+                <a href="/posts/{{ $post->slug }}">
+                    {{ $post->title }}
+                </a>
+            </h1>  
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
     @endforeach
+
+</x-layout>
+
+
     
-@endsection
+    
 
  
