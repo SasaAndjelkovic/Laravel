b7b9d1e9cb7ea2ebc20cea0180643c80b1337c1a
SasaAndjelkovic <sandjelkovickg@gmail.com>
2023-04-23 21:36:35 +0200

    Posts assosiated with a Category

--- section2/routes/web.php
+++ section2/routes/web.php
@@ -1,24 +1,27 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use Illuminate\Support\Facades\File;
 
 Route::get('/', function () {
 
-    $posts = Post::all();
-
     return view('posts', [
-        'posts' => $posts
+        'posts' => Post::all()
     ]);
 });
 
-Route::get('posts/{post:slug}', function(Post $post) //Post::where ('slug', $post)->firstOrFail()
-//Route::get('posts/{post}', function(Post $post) //Post::where ('slug', $post)->firstOrFail()
+Route::get('posts/{post:slug}', function(Post $post) 
 {    
-    
     return view('post', [
         'post' => $post
+    ]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category){
+    return view('posts', [
+        'posts' => $category->posts
     ]);
 });
 
